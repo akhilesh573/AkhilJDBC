afafdb071fd96c6f11fb04006cbc3da37d46b749
akhilesh <akhileshdadge@gmail.com>
2021-04-21 12:30:48 +0530

    mini proj

---
+++ ConnectionFactory.java
@@ -0,0 +1,28 @@
+package com.miniproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ConnectionFactory 
+{
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() throws SQLException 
+    {
+        if(conn==null)
+        {
+            ResourceBundle bundle = ResourceBundle.getBundle("com/miniproject/dbConfig");
+            String dc = bundle.getString("driverClass");
+            String url = bundle.getString("url");
+            String username = bundle.getString("username");
+            String password = null;
+
+           conn = DriverManager.getConnection(url, username, password);
+            
+        }
+        return conn;
+    }
+}
